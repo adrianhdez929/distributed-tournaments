0b07aad0f766e01e6eb7f59fa6c724ad5164fb62
adrianhdez929 <adrianhdez929@gmail.com>
2024-11-28 18:20:33 -0500

    feat: dockerize client app

---
+++ client/games/tictactoe.go
@@ -0,0 +1,71 @@
+package games
+
+import (
+	"shared/interfaces"
+)
+
+type TicTacToe struct {
+	players []interfaces.Player
+	status  interfaces.GameStatus
+	board   [3][3]int
+	winner  interfaces.Player
+}
+
+func NewTicTacToe(players []interfaces.Player) interfaces.Game {
+	board := [3][3]int{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+	return &TicTacToe{players: players, status: interfaces.NotStarted, board: board}
+}
+
+func (g *TicTacToe) Play() {
+	g.status = interfaces.Running
+	lineMade := false
+
+	for !lineMade {
+		for i, player := range g.players {
+			move := player.Move(g.State())
+			g.board[move.X][move.Y] = i
+			g.checkLine(g.board, [2]int{move.X, move.Y})
+		}
+	}
+}
+
+func (g *TicTacToe) Winner() interfaces.Player {
+	return g.winner
+}
+
+func (g *TicTacToe) Status() interfaces.GameStatus {
+	return g.status
+}
+
+func (g *TicTacToe) State() interfaces.GameState {
+	state := make(map[string]interface{})
+	state["board"] = g.board
+	return state
+}
+
+func (g *TicTacToe) checkLine(matrix [3][3]int, coord [2]int) bool {
+	directions := [4][2]int{{1, 0}, {0, 1}, {0, 0}, {1, 1}}
+	for _, direction := range directions {
+		checkCount := 1
+
+		if matrix[abs((coord[0]+direction[0])%2)][abs((coord[1]+direction[1])%2)] == matrix[coord[0]][coord[1]] {
+			checkCount++
+		}
+
+		if matrix[abs((coord[0]-direction[0])%2)][abs((coord[1]-direction[1])%2)] == matrix[coord[0]][coord[1]] {
+			checkCount++
+		}
+
+		if checkCount == 3 {
+			return true
+		}
+	}
+	return false
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
--- client/players/greedy.go
+++ client/players/greedy.go
@@ -2,21 +2,34 @@ package players
 
 import (
 	"fmt"
+	"math/rand"
 	"shared/interfaces"
 )
 
 func NewGreedyPlayer(id int) interfaces.Player {
-	return &GreedyPlayer{id}
+	return &TicTacToeGreedyPlayer{id}
 }
 
-type GreedyPlayer struct {
+type TicTacToeGreedyPlayer struct {
 	id int
 }
 
-func (p *GreedyPlayer) Id() string {
+func (p *TicTacToeGreedyPlayer) Id() string {
 	return fmt.Sprintf("%d", p.id)
 }
 
-func (p *GreedyPlayer) Move() {
-	fmt.Printf("%s is moving\n", p.Id())
+func (p *TicTacToeGreedyPlayer) Move(state interfaces.GameState) interfaces.Move {
+	board := state["board"].([3][3]int)
+	valid := false
+	for !valid {
+		x := rand.Int() % 2
+		y := rand.Int() % 2
+
+		if board[x][y] == 0 {
+			valid = true
+			return interfaces.Move{X: x, Y: y}
+		}
+	}
+
+	return interfaces.Move{X: 0, Y: 0}
 }
