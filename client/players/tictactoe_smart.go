c8b78e38d8a59174ba79a150f1301f96fc7803e1
GigaCronos <jorgepichardocronos@gmail.com>
2025-02-19 00:47:22 -0500

    Fixed bug on tictactoe_smart

--- client/players/tictactoe_smart.go
+++ client/players/tictactoe_smart.go
@@ -18,8 +18,45 @@ func (p *TicTacToeSmartPlayer) Id() string {
 }
 
 func (p *TicTacToeSmartPlayer) Move(state interfaces.GameState) interfaces.Move {
-	
+	board := state["board"].([3][3]int)
+	move,_:=p.BestMove(board,p.id)
+	return move
+}
 
-	return interfaces.Move{X: 2, Y: 3}
+func (p *TicTacToeSmartPlayer) BestMove(board [3][3]int,id int) (interfaces.Move,int){
+	other:=-1
+	for i:=0;i<3;i++{
+		for j:=0;j<3;j++{
+			if board[i][j]!=0 && board[i][j]!=id{
+				other=board[i][j]
+				break
+			}
+		}
+	}
+	ans1:= interfaces.Move{X: -1, Y: -1,Value: id}
+	val:=-1
+	for i:=0;i<3;i++{
+		for j:=0;j<3;j++{
+			if board[i][j]==0{
+				board[i][j]=id
+				_,value :=p.BestMove(board,other)
+				if value==-1{
+					return interfaces.Move{X: i, Y: j,Value: id},1
+				}
+				if value==0{
+					ans1=interfaces.Move{X: i, Y: j,Value: id}
+					val=0
+				}
+				if val==-1{
+					ans1=interfaces.Move{X: i, Y: j,Value: id}
+				}
+			}
+		}
+	}
+	if ans1.X==-1{
+		return ans1,0
+	}
+	return ans1,val
+	
 }
 
